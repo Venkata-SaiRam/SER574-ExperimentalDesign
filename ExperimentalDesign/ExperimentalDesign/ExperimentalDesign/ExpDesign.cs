9ed3226e1ccf3276e581107302c7518702bc7843
venkata sai ram <veadala@asu.edu>
2020-02-25 17:37:25 -0700

    Pushing the dll file along with folders

---
+++ ExperimentalDesign/ExperimentalDesign/ExperimentalDesign/ExpDesign.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ExperimentalDesign
+{
+    public class ExpDesign
+    {
+        public void helloWorld()
+        {
+            
+            Console.WriteLine("Experimental design working");
+            
+        }
+        public String result()
+        {
+            String output = "Experimental design working";
+            return output;
+        }
+    }
+}
