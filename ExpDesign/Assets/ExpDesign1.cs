ced91ece21057c2d1d3c9b59641640a46fe089e4
venkata sai ram <veadala@asu.edu>
2020-02-25 18:49:54 -0700

    Added Unity Application with Build

---
+++ ExpDesign/Assets/ExpDesign1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ExperimentalDesign;
+
+public class ExpDesign1 : MonoBehaviour
+{
+	public InputField nameField;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        ExpDesign ed = new ExpDesign();
+		String finalresult = ed.result();
+		nameField.text = finalresult;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        
+    }
+}
